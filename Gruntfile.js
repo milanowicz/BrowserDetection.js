11d4a0abffee9261558204285d842303d179430f
Milanowicz <amaru-jive@gmx.de>
2014-05-04 11:52:18 +0200

    Project init.

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+    
+    "use strict";
+    
+    grunt.initConfig({
+
+        pkg : grunt.file.readJSON('package.json'),
+
+		devUpdate: {
+            show: {
+                options: {
+                    updateType: 'report',
+                    reportUpdated: true,
+                    packages: {
+                        devDependencies: true,
+                        dependencies: true
+                    },
+					packageJson: null
+                }
+            },
+            install: {
+                options: {
+                    updateType: 'force',
+                    reportUpdated: false,
+                    semver: false,
+                    packages: {
+                        devDependencies: true,
+                        dependencies: false
+                    },
+                    packageJson: null
+                }
+            }
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            gruntfile: {
+                src: 'Gruntfile.js'
+            },
+            src: {
+                src: [
+                    'Detection.js'
+                ]
+            }
+        },
+        uglify : {
+            options : {
+                banner : '/*!\n' +
+                         ' * <%= pkg.name %> - <%= grunt.template.today("yyyy") %> \n' +
+                         ' * Version <%= pkg.version %>\n' +
+                         ' */\n',
+                report : 'min'
+            },
+            Detection : {
+                src : 'BrowserDetection.js',
+                dest : 'BrowserDetection.min.js'
+            }
+        }
+
+    });
+
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-dev-update');
+
+    grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
