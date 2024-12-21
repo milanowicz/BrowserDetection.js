220ddb4c93a3e22cd14662838b289985affb7da3
Milanowicz <amaru-jive@gmx.de>
2024-12-21 22:48:10 +0100

    Update libs

--- dist/BrowserDetection.js
+++ dist/BrowserDetection.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
-
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 /*!
  * BrowserDetection.js Library
  * Author: milanowicz
@@ -24,27 +24,20 @@ module.exports = /*#__PURE__*/function () {
    */
   function BrowserDetection(window, userAgent, appName) {
     _classCallCheck(this, BrowserDetection);
-
     this._name = 'BrowserDetection.js';
-
     if (typeof window === 'undefined') {
       window = {};
     }
-
     this._w = window;
-
     if (typeof userAgent === 'undefined') {
       userAgent = '';
     }
-
     this._userAgent = userAgent;
-
     if (typeof appName === 'undefined') {
       this._appName = '';
     } else {
       this._appName = appName;
     }
-
     this._browserName = '';
     this._isMobile = false;
     this._osType = '';
@@ -54,113 +47,112 @@ module.exports = /*#__PURE__*/function () {
     this._lang = 'en';
     this._langCode = 'en-us';
   }
+
   /**
    * Mobile check variable.
    *
    * @type {boolean}
    */
-
-
-  _createClass(BrowserDetection, [{
+  return _createClass(BrowserDetection, [{
     key: "isMobile",
     value: function isMobile() {
       return this._isMobile;
     }
+
     /**
      * Internet Explorer check variable.
      *
      * @type {boolean}
      */
-
   }, {
     key: "isIE",
     value: function isIE() {
       return this._isIE;
     }
+
     /**
      * Internet Explorer Edge check variable.
      *
      * @type {boolean}
      */
-
   }, {
     key: "isEdge",
     value: function isEdge() {
       return this._isEdge;
     }
+
     /**
      * App Name.
      *
      * @return {string}
      */
-
   }, {
     key: "getAppName",
     value: function getAppName() {
       return this._appName;
     }
+
     /**
      * Browser Type.
      *
      * @type {string}
      */
-
   }, {
     key: "getBrowserName",
     value: function getBrowserName() {
       return this._browserName;
     }
+
     /**
      * Get Mobile OS Device Type
      *
      * @return {string}
      */
-
   }, {
     key: "getOsType",
     value: function getOsType() {
       return this._osType;
     }
+
     /**
      * Internet Explorer Version.
      *
      * @type {number}
      * @global
      */
-
   }, {
     key: "getIeVersion",
     value: function getIeVersion() {
       return this._ieVersion;
     }
+
     /**
      * Browser Language.
      *
      * @type {string}
      */
-
   }, {
     key: "getLanguage",
     value: function getLanguage() {
       return this._lang;
     }
+
     /**
      * Browser Language Code.
      *
      * @type {string}
      */
-
   }, {
     key: "getLanguageCode",
     value: function getLanguageCode() {
       return this._langCode;
     }
+
     /**
      * Browser User Agent.
      *
      * @return {string}
      */
-
   }, {
     key: "getUserAgent",
     value: function getUserAgent() {
@@ -172,7 +164,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.href || '';
     }
   }, {
@@ -181,7 +172,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.hash || '';
     }
   }, {
@@ -190,7 +180,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.host || '';
     }
   }, {
@@ -199,7 +188,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.hostname || '';
     }
   }, {
@@ -208,7 +196,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.origin || '';
     }
   }, {
@@ -217,7 +204,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.pathname || '';
     }
   }, {
@@ -226,7 +212,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.port || '';
     }
   }, {
@@ -235,7 +220,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.protocol || '';
     }
   }, {
@@ -244,7 +228,6 @@ module.exports = /*#__PURE__*/function () {
       if (typeof this._w.location === 'undefined') {
         return '';
       }
-
       return this._w.location.search || '';
     }
   }, {
@@ -277,12 +260,12 @@ module.exports = /*#__PURE__*/function () {
       console.log(this._name + ' | Port', this.getUrlPort());
       console.log(this._name + ' | Protocol', this.getUrlProtocol());
     }
+
     /**
      * Call all Detection functions.
      *
      * @return {void}
      */
-
   }, {
     key: "checkAll",
     value: function checkAll() {
@@ -292,6 +275,7 @@ module.exports = /*#__PURE__*/function () {
       this.checkBrowserInternetExplorer();
       this.checkAllBrowser();
     }
+
     /**
      * Avoid `console` errors in browsers that lack a console.
      *
@@ -299,7 +283,6 @@ module.exports = /*#__PURE__*/function () {
      * @return {void}
      */
     // Stryker disable all
-
   }, {
     key: "consoleWrapper",
     value: function consoleWrapper() {
@@ -307,64 +290,57 @@ module.exports = /*#__PURE__*/function () {
       var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd', 'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'];
       var length = methods.length;
       var console = this._w.console = this._w.console || {};
-
       while (length--) {
-        method = methods[length]; // Only stub undefined methods.
+        method = methods[length];
 
+        // Only stub undefined methods.
         if (!console[method]) {
           console[method] = function () {};
         }
       }
     }
+
     /**
      * Check Operation System from Device.
      *
      * @return {void}
      */
     // Stryker restore all
-
   }, {
     key: "checkOperationSystem",
     value: function checkOperationSystem() {
       this._isMobile = this.checkAllMobiles();
       this._osType = '';
-
       if (this.checkMobileDeviceAndroid()) {
         this._osType = 'Android';
       }
-
       if (this.checkMobileDeviceBlackBerry()) {
         this._osType = 'Black Berry';
       }
-
       if (this.checkMobileDeviceiOS()) {
         this._osType = 'Apple';
       }
-
       if (this.checkMobileDeviceOpera()) {
         this._osType = 'Opera';
       }
-
       if (this.checkMobileDeviceWindows()) {
         this._osType = 'Windows';
       }
-
       if (this._osType !== '') {
         this._isMobile = true;
       } else {
         if (this.checkDesktopDeviceApple()) {
           this._osType = 'Apple';
         }
-
         if (this.checkDesktopDeviceLinux()) {
           this._osType = 'Linux';
         }
-
         if (this.checkDesktopDeviceWindows()) {
           this._osType = 'Windows';
         }
       }
     }
+
     /**
      * Check all Mobile Devices by a RegEx string
      * Mobile Device Detection by http://detectmobilebrowsers.com/
@@ -372,109 +348,107 @@ module.exports = /*#__PURE__*/function () {
      * @return {boolean}
      */
     // Stryker disable all
-
   }, {
     key: "checkAllMobiles",
     value: function checkAllMobiles() {
       return /(tablet|ipad|playbook|silk|android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(this._userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(this._userAgent.substr(0, 4));
-    } // Stryker restore all
+    }
+    // Stryker restore all
 
     /**
      * Check if is an Android device
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkMobileDeviceAndroid",
     value: function checkMobileDeviceAndroid() {
       return !!this._userAgent.match(/Android/i);
     }
+
     /***
      * Check if is a BlackBerry device
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkMobileDeviceBlackBerry",
     value: function checkMobileDeviceBlackBerry() {
       return !!this._userAgent.match(/BlackBerry/i);
     }
+
     /**
      * Check if is an iOS device
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkMobileDeviceiOS",
     value: function checkMobileDeviceiOS() {
       return !!this._userAgent.match(/iPhone|iPad|iPod/i);
     }
+
     /**
      * Check if is an Opera device
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkMobileDeviceOpera",
     value: function checkMobileDeviceOpera() {
       return !!this._userAgent.match(/Opera Mini|Opera Mobi/i);
     }
+
     /**
      * Check if is a Microsoft Windows device
      * @returns {boolean}
      */
-
   }, {
     key: "checkMobileDeviceWindows",
     value: function checkMobileDeviceWindows() {
       return !!this._userAgent.match(/IEMobile|Windows Phone/i);
     }
+
     /**
      * Check if is a Apple device
      * @returns {boolean}
      */
-
   }, {
     key: "checkDesktopDeviceApple",
     value: function checkDesktopDeviceApple() {
       return !!this._userAgent.match(/Macintosh/i);
     }
+
     /**
      * Check if is a GNU/Linux
      * @returns {boolean}
      */
-
   }, {
     key: "checkDesktopDeviceLinux",
     value: function checkDesktopDeviceLinux() {
       return !!this._userAgent.match(/Linux/i);
     }
+
     /**
      * Check if is a Microsoft Windows device
      * @returns {boolean}
      */
-
   }, {
     key: "checkDesktopDeviceWindows",
     value: function checkDesktopDeviceWindows() {
       return !!this._userAgent.match(/Windows NT/i);
     }
+
     /**
      * Check Browser Default Language.
      *
      * @return {void}
      */
-
   }, {
     key: "checkLanguage",
     value: function checkLanguage() {
       try {
         var t = this;
-
         (function (a) {
           t._lang = a.substr(0, 2).toLowerCase();
           t._langCode = a.toLowerCase();
@@ -482,16 +456,15 @@ module.exports = /*#__PURE__*/function () {
       } catch (e) {
         this._lang = 'en';
         this._langCode = 'en-us';
-
         this._w.console.log(e);
       }
     }
+
     /**
      * Check all Browser and save type in global variable
      *
      * @return {void}
      */
-
   }, {
     key: "checkAllBrowser",
     value: function checkAllBrowser() {
@@ -507,109 +480,108 @@ module.exports = /*#__PURE__*/function () {
         this._browserName = 'Safari';
       }
     }
+
     /**
      * Check if is a Google Chrome browser
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkBrowserChrome",
     value: function checkBrowserChrome() {
       return !!this._userAgent.match(/Chrome/i);
     }
+
     /**
      * Check if is a Mozilla Firefox browser
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkBrowserFirefox",
     value: function checkBrowserFirefox() {
       return !!this._userAgent.match(/Firefox/i);
     }
+
     /**
      * Check if is an Opera browser
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkBrowserOpera",
     value: function checkBrowserOpera() {
       return !!this._userAgent.match(/Opera/i);
     }
+
     /**
      * Check if is an Apple Safari browser
      *
      * @returns {boolean}
      */
-
   }, {
     key: "checkBrowserSafari",
     value: function checkBrowserSafari() {
       return !!this._userAgent.match(/Safari/i);
     }
+
     /**
      * Detection from Internet Explorer and his version.
      *
      * @return {void}
      */
-
   }, {
     key: "checkBrowserInternetExplorer",
     value: function checkBrowserInternetExplorer() {
       var RegEx, HtmlTag;
       var t = this;
+
       /**
        * Find version number by regEx through the userAgent
        * Then set it to a variable and to the HTML Element Tag
        */
-
       function check(RegEx) {
         if (RegEx.exec(t._userAgent) !== null) {
-          t._isIE = true; // Extract version number
+          t._isIE = true;
 
-          t._ieVersion = parseInt(RegExp.$1); // Get HTML tag element
+          // Extract version number
+          t._ieVersion = parseInt(RegExp.$1);
 
+          // Get HTML tag element
           try {
-            HtmlTag = t._w.document.getElementsByTagName('html')[0]; // Append html tag by an ie class
+            HtmlTag = t._w.document.getElementsByTagName('html')[0];
 
+            // Append html tag by an ie class
             HtmlTag.className = HtmlTag.className || "";
             HtmlTag.className += ' ie' + t._ieVersion;
           } catch (e) {}
-
           return true;
         }
-
         return false;
       }
+
       /**
        * Check Internet Explorer under equal Version 10
        */
-
-
       if (this._appName === 'Microsoft Internet Explorer') {
         // RegEx to find the IE version number
         RegEx = new RegExp("MSIE ([0-9][.0-9])");
         check(RegEx);
+
         /**
          * Check Internet Explorer OVER EQUAL Version 11
          */
       } else {
         // RegEx to find the IE version number
         RegEx = new RegExp("Trident/.*rv:([0-9][.0-9])");
-        check(RegEx); // Check if Browser is Edge
+        check(RegEx);
 
+        // Check if Browser is Edge
         RegEx = new RegExp("Edge\/([0-9][.0-9])");
-
         if (check(RegEx)) {
           t._isEdge = true;
         }
       }
     }
   }]);
-
-  return BrowserDetection;
 }();
